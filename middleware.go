70ff9c18554d7e72deda07ed2712e183987cb336
Rene Zbinden <rene.zbinden@gmail.com>
2015-04-07 13:20:54 +0200

    change: get db from session

--- middleware.go
+++ middleware.go
@@ -8,29 +8,26 @@ import (
 )
 
 const (
-	ContextKey = "database" // ContextKey defines the key under wich the the database reference is stored in the request context.
+	ContextKey = "mongosession" // ContextKey defines the key under wich the the database reference is stored in the request context.
 )
 
 // MongoMiddleware is a middleware handler that stores a mongo database reference in the request context.
 type MongoMiddleware struct {
-	database string
-	Session  *mgo.Session
+	Session *mgo.Session
 }
 
 // NewMongoMiddleware retunrs a new MongoMiddleware instance.
-func NewMongoMiddleware(url string, database string) *MongoMiddleware {
-	session, err := mgo.Dial(url)
+func NewMongoMiddleware(uri string) *MongoMiddleware {
+	session, err := mgo.Dial(uri)
 	if err != nil {
 		panic(err)
 	}
-	session.DB(database)
-	return &MongoMiddleware{Session: session, database: database}
+	return &MongoMiddleware{Session: session}
 }
 
 func (m *MongoMiddleware) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	reqSession := m.Session.Clone()
 	defer reqSession.Close()
-	db := reqSession.DB(m.database)
-	httpcontext.Set(r, ContextKey, db)
+	httpcontext.Set(r, ContextKey, m.Session)
 	next(rw, r)
 }
